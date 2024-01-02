2b4d04ad668bffadee1be329769bcc4d241ce818
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2024-01-02 15:41:53 +0530

    webengage commit

---
+++ android/app/src/main/kotlin/com/example/stoxhero/MainApplication.kt
@@ -0,0 +1,17 @@
+import io.flutter.app.FlutterApplication;
+import com.webengage.webengage_plugin.WebengageInitializer;
+import com.webengage.sdk.android.WebEngageConfig;
+import com.webengage.sdk.android.WebEngage;
+
+class MainApplication : FlutterApplication() {
+
+override fun onCreate() {
+        super.onCreate()
+        val webEngageConfig = WebEngageConfig.Builder()
+            .setWebEngageKey("in~~c2ab36a9")
+            .setAutoGCMRegistrationFlag(false)
+            .setDebugMode(true) // only in development mode
+            .build()
+        WebengageInitializer.initialize(this, webEngageConfig)
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:stoxhero/firebase_options.dart';
-
+import 'package:webengage_flutter/webengage_flutter.dart';
 import 'src/app/app.dart';
 
-const bool isProd = true;
-const bool useTestToken = false;
+const bool isProd = false;
+const bool useTestToken = true;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WebEngagePlugin _webEngagePlugin = new WebEngagePlugin();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
