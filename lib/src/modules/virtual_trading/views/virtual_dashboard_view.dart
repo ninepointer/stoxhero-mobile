8fb24a3f96b476b075a7d300eb00ecba1bae063a
Harshit kumar yadav <harshitkmryadav@gmail.com>
2024-01-19 02:48:19 +0530

    All Dark Mode Implemented

--- lib/src/modules/virtual_trading/views/virtual_dashboard_view.dart
+++ lib/src/modules/virtual_trading/views/virtual_dashboard_view.dart
@@ -158,7 +158,7 @@ class FutureAndOptionDashBoard extends StatelessWidget {
                     bottom: 0,
                     child: Container(
                       decoration: BoxDecoration(
-                        border: Border.all(color: AppColors.grey, width: 1.0),
+                        // border: Border.all(color: AppColors.grey, width: 1.0),
                         borderRadius: BorderRadius.circular(8),
                       ),
                     ),
